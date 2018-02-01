2f607f6aa1c6a6bb90b050a3eebda994a1646c2c
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-02-01 16:04:45 +0700

    refromat hasil

--- routes/bills.js
+++ routes/bills.js
@@ -59,8 +59,9 @@ router.get('/:userId', function(req, res, next) {
 });
 
 router.get('/:userId/:stats', function(req, res, next) {
+  var group = {};
   console.log("S:getQueuedBill called");
-  var sql = `select a.ID_TAGIHAN,a.HARGA,b.NAMA as NAMA_PELANGGAN, c.NAMA as NAMA_LAYANAN from tagihan a
+  var sql = `select a.ID_TAGIHAN,a.HARGA,b.NCLI,b.NAMA as NAMA_PELANGGAN, c.NAMA as NAMA_LAYANAN from tagihan a
   left join pelanggan b
   on a.NCLI = b.NCLI
   left join layanan c
@@ -68,8 +69,19 @@ router.get('/:userId/:stats', function(req, res, next) {
   where b.NO_TELEPON='`+ req.params.userId+`' or b.NO_INTERNET='`+ req.params.userId+`' and a.STATS = `+req.params.stats;
   
     connection.query(sql, function (err, rows, fields) {
+      if(rows.length > 0){
+        group.id = rows[0].NCLI;
+        group.nama = rows[0].NAMA_PELANGGAN;
+        group.total = 0;
+        group.details = [];
+        for (var i = 0; i < rows.length; i++) {
+          group.details.push(rows[i]);
+          group.total += rows[i].HARGA;
+        }
+      }
+      
       if (err) throw err
-      res.json(rows);
+      res.json(group);
     });
 });
 
