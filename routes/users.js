6d4680167d9c705759489ceb2b96ea2e2809a24b
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2018-03-02 04:26:48 +0700

    user login API

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var dotenv = require('dotenv').config({path: '../.env'});
+var debug = require('debug')("node-enigma:users");
+
+var AccessLevel = {
+  BANNED: 0,
+  USER: 1,
+  SUPERUSER: 7,
+  properties: {
+    0: {name: "banned", value: 0},
+    1: {name: "user", value: 1},
+    7: {name: "superuser", value: 7}
+  }
+};
+
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_NAME
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
-
 router.post('/login', function(req, res, next){
+  debug("post login");
+  let username = req.body.username;
+  let password = req.body.password;
   let obj = {};
-  if((req.body.username == 'nanda') && (req.body.password == '123456')){
-    obj = {
-      username : 'nanda',
-      password : '123456',
-      type : 'superuser',
-      login : true
+
+  var sql = `select fullname, department, status from users where username = ? and password = ?`;
+  connection.query(sql,[username,password], function(err, rows, fields){
+    if (err) throw err
+    console.log(rows);
+    let user = rows[0];
+    if(rows.length == 0){
+      obj = {
+        login : false,
+        message : "Login failed"
+      }
+    }else{
+      obj = {
+        fullname : user.fullname,
+        department : user.department,
+        access : AccessLevel.properties[user.status].name,
+        login : true
+      }
     }
-  }else{
-    obj = {
-      login : false,
-      message : 'Login failed'
-    }
-  }
-  console.log(req.body);
-  res.json(obj);
-})
+    console.log(obj);
+    res.json(obj);
+  });
+
+  
+  
+  
+});
+
+module.exports = router;
